a82b94b437baeb2a6f0a10fcdbb426dcf91b6410
underdolphin <underdolphin@users.noreply.github.com>
2019-08-21 01:15:29 +0900

    Update source tree structure.

--- packages/vv-cli/__tests__/vv-cli.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const vvCli = require('..');
-
-describe('vv-cli', () => {
-    it('needs tests');
-});
--- packages/vv-test/__tests__/vv-test.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const vvTest = require('..');
-
-describe('vv-test', () => {
-    it('needs tests');
-});
