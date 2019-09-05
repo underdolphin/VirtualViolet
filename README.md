480591beab6a3c7424ff3df07013a310f43c7da5
underdolphin <underdolphin@users.noreply.github.com>
2019-09-06 01:18:55 +0900

    Clear repostitory,

--- README.md
+++
@@ -1,2 +0,0 @@
-# VirtualViolet
-Virtual Violet or VV is a standard programming language. Do not visual programming.
--- packages/compiler/src/vv_tokens.ts
+++
@@ -1,6 +0,0 @@
-import { createToken } from 'chevrotain';
-
-
-export class VVTokes {
-  public WhiteSpace = createToken({ name: 'Whitespace' });
-}
